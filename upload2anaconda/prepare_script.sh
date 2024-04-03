d7ce1a7eab686aa3c7069ed16cd82f1848a8c1d4
wshuai294 <wshuai294@gmail.com>
2024-04-03 15:54:23 +0800

    conda build

---
+++ upload2anaconda/prepare_script.sh
@@ -0,0 +1,4 @@
+cp /mnt/d/breakpoints/build_localhgt/LocalHGT/Makefile construction/LocalHGT
+cp ../scripts/*py construction/LocalHGT
+cp ../scripts/*sh construction/LocalHGT
+cp ../src/extract_ref_normal_peak.cpp construction/LocalHGT
