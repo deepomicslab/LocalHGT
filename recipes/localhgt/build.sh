908ee389de40cfa1b55951370b541f0128f1916a
wshuai294 <wshuai294@gmail.com>
2024-04-10 16:22:18 +0800

    refine bioconda config

--- recipes/localhgt/build.sh
+++ recipes/localhgt/build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-export C_INCLUDE_PATH=${PREFIX}/include
-export LIBRARY_PATH=${PREFIX}/lib
+# Ensure we run successfully using either conda-forge or defaults ncurses
+# (unlike other platforms, the latter does not automatically pull in libtinfo)
+make 
 
-make
-# make
 mkdir -p $PREFIX/bin
+
 cp $SRC_DIR/scripts/*py $PREFIX/bin
 cp $SRC_DIR/scripts/*sh $PREFIX/bin
 cp $SRC_DIR/scripts/extract_ref $PREFIX/bin
